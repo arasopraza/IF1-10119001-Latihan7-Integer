3bb8892abcf19d83fdc96e054508ddbf90f715b2
arasopraza <aras.opraza@gmail.com>
2020-10-16 21:06:08 +0700

    Initial Commit :tada:

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * @author
+ * NAMA     : Arsy Opraza Akma
+ * KELAS    : IF1
+ * NIM      : 10119001
+ * Deskripsi Program: Program ini berisi perintah untuk menampilkan tipe data integer
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+
+    }
+}
